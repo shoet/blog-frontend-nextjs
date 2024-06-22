4016a5d53391a7d289188034300030bd49f9c0dc
shoet <32211937+shoet@users.noreply.github.com>
2024-06-22 22:31:01 +0900

    Storybookを設定する

---
+++ src/app/_components/Atoms/Badge/index.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Badge } from ".";
+
+export default {
+  title: "Atoms/Badge",
+  component: Badge,
+} as Meta<typeof Badge>;
+
+export type Story = StoryObj<typeof Badge>;
+
+export const Default: Story = {};
