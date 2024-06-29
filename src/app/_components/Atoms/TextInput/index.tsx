3d75fc518b7df55af245461402fd8edc0e4e2a1c
shoet <32211937+shoet@users.noreply.github.com>
2024-06-29 12:36:20 +0900

    TextInputコンポーネントを作成

---
+++ src/app/_components/Atoms/TextInput/index.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps, CSSProperties } from "react";
+import css from "./index.module.scss";
+import clsx from "clsx";
+
+type TextInputProps = {
+  hasBorder?: boolean;
+  isError?: boolean;
+} & ComponentProps<"input">;
+
+export const TextInput = (props: TextInputProps) => {
+  const { hasBorder = true, isError = false, ...rest } = props;
+
+  const style = {
+    "--border-color": "gray",
+    "--border-color-error": "red",
+  } as CSSProperties;
+
+  return (
+    <input
+      className={clsx(
+        hasBorder && css["input--has-border"],
+        isError && css["input--has-border--error"],
+      )}
+      style={style}
+      type="text"
+      {...rest}
+    />
+  );
+};
