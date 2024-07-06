26c31ae1233bf077ed1eb3f54eae3c9aca180043
shoet <32211937+shoet@users.noreply.github.com>
2024-07-06 16:15:52 +0900

    ページの骨組みを作成する

--- src/app/(site)/blogs/page.tsx
+++ src/app/(site)/blogs/page.tsx
@@ -1,14 +1,3 @@
-import { Badge } from "@/app/_components/Atoms/Badge";
-import { IconGitHub } from "@/app/_components/Atoms/Icon";
-
-export default function BlogHomePage() {
-  // Todo: ブログの一覧取得
-  return (
-    <div>
-      <Badge backgroundColor="green" color="white" focusColor="darkgreen">
-        hoge
-      </Badge>
-      <IconGitHub focusColor="#7ccca4" size="4x" />
-    </div>
-  );
+export default async function BlogHomePage() {
+  return <div>blog home</div>;
 }
