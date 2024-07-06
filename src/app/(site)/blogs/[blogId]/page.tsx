c3a7e561d9584ea2e255811d75e1a4f0853709b7
shoet <32211937+shoet@users.noreply.github.com>
2024-07-06 16:24:08 +0900

    Metadataを追加

--- src/app/(site)/blogs/[blogId]/page.tsx
+++ src/app/(site)/blogs/[blogId]/page.tsx
@@ -1,10 +1,30 @@
+import { getBlogDetail } from "@/services/getBlogDetail";
+import { Metadata, ResolvingMetadata } from "next";
+
 type BlogDetailPageProps = {
   params: {
-    blogId: string;
+    blogId: number;
   };
   searchParams: {};
 };
 
-export default function BlogDetailPage(props: BlogDetailPageProps) {
-  return <div>詳細ページ: {props.params.blogId}</div>;
-}
+export const generateMetadata = async (
+  props: BlogDetailPageProps,
+  parent: ResolvingMetadata,
+): Promise<Metadata> => {
+  const { title } = await parent;
+  const { blogId } = props.params;
+  const blog = await getBlogDetail(blogId);
+  return {
+    title: `${blog.title} | ${title?.absolute}`,
+    description: blog.description,
+  };
+};
+
+const BlogDetailPage = async (props: BlogDetailPageProps) => {
+  const { blogId } = props.params;
+  const blog = await getBlogDetail(blogId);
+  return <div>{blog.title}</div>;
+};
+
+export default BlogDetailPage;
--- src/app/(site)/blogs/page.tsx
+++ src/app/(site)/blogs/page.tsx
@@ -1,3 +1,16 @@
+import { Metadata, ResolvingMetadata } from "next";
+
+export const generateMetadata = async (
+  _: any,
+  parent: ResolvingMetadata,
+): Promise<Metadata> => {
+  const { title } = await parent;
+  return {
+    title: `直近のブログ一覧 | ${title?.absolute}`,
+    description: "直近のブログ一覧",
+  };
+};
+
 export default async function BlogHomePage() {
   return <div>blog home</div>;
 }
