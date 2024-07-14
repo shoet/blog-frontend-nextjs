8742302bb084f231ec7e8ed6ec751b374d47fda6
shoet <32211937+shoet@users.noreply.github.com>
2024-07-14 15:13:25 +0900

    ClientKeywordSearchFormを作成する

---
+++ src/app/_components/Organisms/ClientKeywordSearchForm/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { SearchForm } from "../../Molecules/SearchForm";
+
+export const ClientKeywordSearchForm = () => {
+  const navigation = useRouter();
+  const handleOnSubmitKeyword = (keyword: string) => {
+    navigation.push(`/blogs/search?keyword=${keyword}`);
+  };
+  return <SearchForm onSubmit={handleOnSubmitKeyword} />;
+};
