fb950f3a1eeca62e3d65af00e7cfd73fa6e2298d
shoet <32211937+shoet@users.noreply.github.com>
2024-07-14 16:34:00 +0900

    クリック操作追加

--- src/app/_components/Molecules/ClientBlogCardList/index.tsx
+++ src/app/_components/Molecules/ClientBlogCardList/index.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Blog } from "@/types/api";
 import { BlogCard } from "../BlogCard";
 import css from "./index.module.scss";
+import { useRouter } from "next/navigation";
 
 type ClientBlogCardListProps = {
   blogs: Blog[];
@@ -8,10 +10,18 @@ type ClientBlogCardListProps = {
 
 export const ClientBlogCardList = (props: ClientBlogCardListProps) => {
   const { blogs } = props;
+  const router = useRouter();
+  const handleOnClickCard = (blog: Blog) => {
+    router.push(`/blogs/${blog.id}`);
+  };
   return (
     <div className={css.blogs}>
       {blogs.map((b) => {
-        return <BlogCard key={b.id} blog={b} />;
+        return (
+          <div key={b.id} onClick={() => handleOnClickCard(b)}>
+            <BlogCard blog={b} />
+          </div>
+        );
       })}
     </div>
   );
