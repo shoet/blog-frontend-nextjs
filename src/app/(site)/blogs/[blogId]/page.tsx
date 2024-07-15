67e42afd0628bfc691b886e6df85cd27d58e1ff2
shoet <32211937+shoet@users.noreply.github.com>
2024-07-15 14:26:14 +0900

    詳細画面の表示をする

--- src/app/(site)/blogs/[blogId]/page.tsx
+++ src/app/(site)/blogs/[blogId]/page.tsx
@@ -1,5 +1,9 @@
 import { getBlogDetail } from "@/services/getBlogDetail";
 import { Metadata, ResolvingMetadata } from "next";
+import css from "./page.module.scss";
+import { Badge } from "@/app/_components/Atoms/Badge";
+import { toStringYYYYMMDD_HHMMSS } from "@/utils/date";
+import { Spacer } from "@/app/_components/Atoms/Spacer";
 
 type BlogDetailPageProps = {
   params: {
@@ -24,7 +28,33 @@ export const generateMetadata = async (
 const BlogDetailPage = async (props: BlogDetailPageProps) => {
   const { blogId } = props.params;
   const blog = await getBlogDetail(blogId);
-  return <div>{blog.title}</div>;
+  return (
+    <div>
+      <div className={css.title}>{blog.title}</div>
+      <Spacer height={20} />
+      <div className={css.descriptionArea}>
+        <div className={css.date}>{toStringYYYYMMDD_HHMMSS(blog.created)}</div>
+        <Spacer width={20} />
+        {blog.tags && (
+          <div className={css.tags}>
+            {blog.tags.map((tag) => {
+              return <Badge key={tag}>{tag}</Badge>;
+            })}
+          </div>
+        )}
+      </div>
+      <Spacer height={20} />
+      <div className={css.imageWrapper}>
+        <img
+          className={css.image}
+          src={blog.thumbnailImageFileName}
+          alt={blog.title}
+        />
+      </div>
+      <Spacer height={20} />
+      <div className={css.content}>{blog.content}</div>
+    </div>
+  );
 };
 
 export default BlogDetailPage;
