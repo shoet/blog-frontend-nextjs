675ece18b7a3b05c539d181f00955f4d0e65ae30
shoet <32211937+shoet@users.noreply.github.com>
2024-07-21 14:40:22 +0900

    将来的な以降のためにMDXでのMarkdownを実装する

---
+++ src/utils/html.ts
@@ -0,0 +1,16 @@
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeStringify from "rehype-stringify";
+
+export async function toHTML(markdown: string): Promise<string> {
+  const htmlText = await unified()
+    .use(remarkParse) // Convert into markdown AST
+    .use(remarkRehype) // Transform to HTML AST
+    .use(rehypeSanitize) // Sanitize HTML input
+    .use(rehypeStringify) // Convert AST into serialized HTML
+    .process(markdown);
+
+  return htmlText.toString();
+}
