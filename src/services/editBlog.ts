1f504be84f42d234b2e004d1c08f6609f91b87f4
shoet <32211937+shoet@users.noreply.github.com>
2024-08-04 18:06:47 +0900

    ブログ編集APIを追加する

---
+++ src/services/editBlog.ts
@@ -0,0 +1,48 @@
+import { Blog } from "@/types/api";
+import { getAPIPath, handleFailed, handleSuccess } from ".";
+import { getServerSideCookie } from "@/utils/cookie";
+
+export async function editBlog(props: {
+  id: number;
+  authorID: number;
+  title: string;
+  content: string;
+  description: string;
+  thumbnailImageFileURL: string;
+  isPublic: boolean;
+  tags: string[];
+}): Promise<Blog> {
+  const {
+    id,
+    authorID,
+    title,
+    content,
+    description,
+    thumbnailImageFileURL,
+    isPublic,
+    tags,
+  } = props;
+  const body: { [key: string]: any } = {
+    id,
+    authorId: authorID,
+    title,
+    content,
+    description,
+    thumbnailImageFileName: thumbnailImageFileURL,
+    isPublic,
+    tags,
+  };
+  const token = getServerSideCookie("authToken")?.value;
+  if (!token) {
+    throw new Error("ログインしてください");
+  }
+  return fetch(getAPIPath(`/blogs/${id}`), {
+    method: "PUT",
+    body: JSON.stringify(body),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then(handleSuccess)
+    .catch(handleFailed);
+}
