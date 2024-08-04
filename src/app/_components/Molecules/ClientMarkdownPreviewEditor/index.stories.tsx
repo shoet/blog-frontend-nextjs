86c27d2f6dee46066af70ed76180f9bf19f866b0
shoet <32211937+shoet@users.noreply.github.com>
2024-08-04 14:36:51 +0900

    ClientMarkdownPreviewTextAreaをファイルDrop可能にする

--- src/app/_components/Molecules/ClientMarkdownPreviewEditor/index.stories.tsx
+++ src/app/_components/Molecules/ClientMarkdownPreviewEditor/index.stories.tsx
@@ -1,11 +1,36 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { MarkdownPreviewTextArea } from ".";
+import { ClientMarkdownPreviewTextArea } from ".";
+import { useState } from "react";
 
 export default {
-  title: "Molecules/ClientMarkdownPreviewEditor",
+  title: "Molecules/ClientMarkdownPreviewTextArea",
   component: ClientMarkdownPreviewTextArea,
 } as Meta<typeof ClientMarkdownPreviewTextArea>;
 
 export type Story = StoryObj<typeof ClientMarkdownPreviewTextArea>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {},
+  render: (args) => {
+    const [markdown, setMarkdown] = useState("");
+
+    const handleOnDragDrop = (file: File) => {
+      console.log("### drop");
+      const fileName = file.name;
+      setMarkdown(`${markdown}\n${fileName}`);
+    };
+
+    const handleChangeText = (text: string) => {
+      setMarkdown(text);
+    };
+
+    return (
+      <ClientMarkdownPreviewTextArea
+        {...args}
+        markdownText={markdown}
+        onChange={handleChangeText}
+        onDragDrop={handleOnDragDrop}
+      />
+    );
+  },
+};
--- src/app/_components/Molecules/ClientMarkdownPreviewEditor/index.tsx
+++ src/app/_components/Molecules/ClientMarkdownPreviewEditor/index.tsx
@@ -1,30 +1,43 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MarkdownRenderer } from "../MarkdownRenderer";
 import css from "./index.module.scss";
 import clsx from "clsx";
-import { TextArea } from "../../Atoms/TextArea";
+import { DragableTextarea } from "../../Atoms/DragableTextarea";
 
 type ClientMarkdownPreviewTextAreaProps = {
   markdownText?: string;
   onChange?: (text: string) => void;
+  onDragDrop?: (file: File) => void;
 };
 
 export const ClientMarkdownPreviewTextArea = (
   props: ClientMarkdownPreviewTextAreaProps,
 ) => {
-  const { markdownText = "", onChange } = props;
+  const { markdownText = "", onChange, onDragDrop } = props;
   const [text, setText] = useState(markdownText);
+
+  useEffect(() => {
+    setText(markdownText);
+  }, [markdownText]);
 
   const handleChangeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.target.value);
     onChange?.(e.target.value);
   };
 
+  const handleDragDrop = (file: File) => {
+    onDragDrop?.(file);
+  };
+
   return (
     <div className={css.markdownPreviewEditor}>
       <div className={clsx(css.editor, css.textEditor)}>
-        <TextArea onChange={handleChangeText} />
+        <DragableTextarea
+          value={markdownText}
+          onChange={handleChangeText}
+          onDragDrop={handleDragDrop}
+        />
       </div>
       <div className={css.preview}>
         <MarkdownRenderer markdown={text} />
