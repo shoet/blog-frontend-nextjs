8275e0545481bd9bc3355ccd72d769bd5a7bf878
shoet <32211937+shoet@users.noreply.github.com>
2024-08-17 22:21:47 +0900

    検索APIをオフセットページングに切り替える

--- src/services/getBlogs.ts
+++ src/services/getBlogs.ts
@@ -8,23 +8,28 @@ type GetBlogsResponse = {
 };
 
 export async function getBlogs({
+  page = 1,
   limit = 10,
   tag,
   keyword,
 }: {
+  page?: number;
   limit?: number;
   tag?: string;
   keyword?: string;
 }): Promise<GetBlogsResponse> {
   const urlParams = new URLSearchParams();
   urlParams.append("limit", limit.toString());
+  urlParams.append("page", page.toString());
   if (tag) {
     urlParams.append("tag", tag);
   }
   if (keyword) {
     urlParams.append("keyword", keyword);
   }
-  return fetch(getAPIPath(`/blogs?${urlParams.toString()}`), { method: "GET" })
+  return fetch(getAPIPath(`/v2/blogs?${urlParams.toString()}`), {
+    method: "GET",
+  })
     .then(handleSuccess)
     .catch(handleFailed);
 }
