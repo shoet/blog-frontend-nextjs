3ce4a2aabbffb9aaa011404a22ee508f0ee3cbfe
shoet <32211937+shoet@users.noreply.github.com>
2025-02-08 12:03:41 +0900

    ログイン画面にバリデーションエラー以外のエラーを出力する

--- src/app/(admin)/_components/Organisms/LoginActionForm/index.tsx
+++ src/app/(admin)/_components/Organisms/LoginActionForm/index.tsx
@@ -5,6 +5,9 @@ import { TextInput } from "@/app/_components/Atoms/TextInput";
 import { Spacer } from "@/app/_components/Atoms/Spacer";
 import { Button } from "@/app/_components/Atoms/Button";
 
+/**
+ * ValidationError は与えられたerrorsからfieldに対するエラーメッセージコンポーネントを生成する
+ */
 const ValidationError = ({
   errors,
   field,
@@ -17,29 +20,48 @@ const ValidationError = ({
   return <ErrorText>{error.error}</ErrorText>;
 };
 
+const Errors = ({ errors }: { errors: string[] | undefined }) => {
+  if (errors === undefined || errors?.length === 0) return null;
+  return (
+    <div>
+      {errors.map((error) => {
+        return <ErrorText key={error}>{error}</ErrorText>;
+      })}
+      <Spacer height={30} />
+    </div>
+  );
+};
+
 export const LoginActionForm = ({
   formAction,
   errors,
+  validateErrors,
+  isLoading = false,
 }: {
   formAction: (formData: FormData) => void;
-  errors?: ZodValidateError[];
+  errors?: string[];
+  validateErrors?: ZodValidateError[];
+  isLoading: boolean;
 }) => {
   return (
     <form action={formAction}>
       <div className={css.formArea}>
+        <Errors errors={errors} />
         <div>
           <TextInput name="email" placeholder="Email" />
-          <ValidationError errors={errors} field="email" />
+          <ValidationError errors={validateErrors} field="email" />
         </div>
         <Spacer height={20} />
         <div>
           <TextInput name="password" placeholder="Password" type="password" />
-          <ValidationError errors={errors} field="password" />
+          <ValidationError errors={validateErrors} field="password" />
         </div>
       </div>
       <Spacer height={30} />
       <div className={css.actionArea}>
-        <Button variant="primary">Signin</Button>
+        <Button variant="primary" disabled={isLoading}>
+          Signin
+        </Button>
       </div>
     </form>
   );
