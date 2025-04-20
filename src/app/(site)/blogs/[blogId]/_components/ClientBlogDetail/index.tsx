b780543d6d1d17247aad878aa719bcaecc4a51ae
shoet <32211937+shoet@users.noreply.github.com>
2025-04-20 22:18:35 +0900

    ブログ記事をClientComponentに切り出す

---
+++ src/app/(site)/blogs/[blogId]/_components/ClientBlogDetail/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Blog } from "@/types/api";
+import css from "./index.module.scss";
+import { Spacer } from "@/app/_components/Atoms/Spacer";
+import { toStringYYYYMMDD_HHMMSS } from "@/utils/date";
+import { Badge } from "@/app/_components/Atoms/Badge";
+import { useTableOfContentContext } from "@/app/_components/Organisms/TableOfContentProvider";
+import { useEffect } from "react";
+import { MarkdownRenderer } from "@/app/_components/Molecules/MarkdownRenderer";
+
+export const ClientBlogDetail = (props: { blog: Blog }) => {
+  const { blog } = props;
+  const { loadHeadings, cleanupHeadings, watchRef } =
+    useTableOfContentContext();
+
+  useEffect(() => {
+    return () => {
+      cleanupHeadings();
+    };
+  }, []);
+
+  const onEndLoadMarkdown = () => {
+    loadHeadings();
+  };
+
+  return (
+    <div>
+      <div className={css.title}>{blog.title}</div>
+      <Spacer height={20} />
+      <div className={css.descriptionArea}>
+        <div className={css.date}>{toStringYYYYMMDD_HHMMSS(blog.created)}</div>
+        <Spacer width={20} />
+        {blog.tags && (
+          <div className={css.tags}>
+            {blog.tags.map((tag) => {
+              return <Badge key={tag}>{tag}</Badge>;
+            })}
+          </div>
+        )}
+      </div>
+      <Spacer height={20} />
+      <div className={css.imageWrapper}>
+        <img
+          className={css.image}
+          src={blog.thumbnailImageFileName}
+          alt={blog.title}
+        />
+      </div>
+      <Spacer height={20} />
+      <div ref={watchRef}>
+        <div id="article">
+          <MarkdownRenderer
+            markdown={blog.content}
+            onEndLoad={onEndLoadMarkdown}
+          />
+        </div>
+      </div>
+      <Spacer height={50} />
+    </div>
+  );
+};
--- src/app/(site)/blogs/[blogId]/page.tsx
+++ src/app/(site)/blogs/[blogId]/page.tsx
@@ -1,10 +1,6 @@
 import { getBlogDetail } from "@/services/getBlogDetail";
 import { Metadata, ResolvingMetadata } from "next";
-import css from "./page.module.scss";
-import { Badge } from "@/app/_components/Atoms/Badge";
-import { toStringYYYYMMDD_HHMMSS } from "@/utils/date";
-import { Spacer } from "@/app/_components/Atoms/Spacer";
-import { ClientMarkdownView } from "@/app/_components/Molecules/ClientMarkdownView";
+import { ClientBlogDetail } from "./_components/ClientBlogDetail";
 
 type BlogDetailPageProps = {
   params: Promise<{
@@ -29,35 +25,7 @@ export const generateMetadata = async (
 const BlogDetailPage = async (props: BlogDetailPageProps) => {
   const { blogId } = await props.params;
   const blog = await getBlogDetail(blogId);
-  return (
-    <div>
-      <div className={css.title}>{blog.title}</div>
-      <Spacer height={20} />
-      <div className={css.descriptionArea}>
-        <div className={css.date}>{toStringYYYYMMDD_HHMMSS(blog.created)}</div>
-        <Spacer width={20} />
-        {blog.tags && (
-          <div className={css.tags}>
-            {blog.tags.map((tag) => {
-              return <Badge key={tag}>{tag}</Badge>;
-            })}
-          </div>
-        )}
-      </div>
-      <Spacer height={20} />
-      <div className={css.imageWrapper}>
-        <img
-          className={css.image}
-          src={blog.thumbnailImageFileName}
-          alt={blog.title}
-        />
-      </div>
-      <Spacer height={20} />
-      <div id="article">
-        <ClientMarkdownView text={blog.content} />
-      </div>
-    </div>
-  );
+  return <ClientBlogDetail blog={blog} />;
 };
 
 export default BlogDetailPage;
