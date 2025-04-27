53e209572b78c3f0a248f4094dee167c9a25565e
shoet <32211937+shoet@users.noreply.github.com>
2025-04-27 10:31:37 +0900

    Avatar画像を実装する

---
+++ src/app/_components/Molecules/AvatarImage/index.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { AvatarImage } from ".";
+
+export default {
+  title: "Molecules/AvatarImage",
+  component: AvatarImage,
+} as Meta<typeof AvatarImage>;
+
+type Story = StoryObj<typeof AvatarImage>;
+
+export const Default: Story = {
+  args: {
+    imageURL: "https://placehold.co/600x400?text=Hello+World",
+  },
+  render: (args) => {
+    return (
+      <div style={{ height: "200px", backgroundColor: "gray" }}>
+        <AvatarImage {...args} size={"200px"} />
+      </div>
+    );
+  },
+};
