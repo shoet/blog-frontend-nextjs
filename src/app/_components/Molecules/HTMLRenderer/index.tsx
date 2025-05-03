33220a1a0eb1b3f96fa79635d3fc520f65025bbb
shoet <32211937+shoet@users.noreply.github.com>
2025-05-03 15:59:25 +0900

    HTMLの描画工程を修正する

--- src/app/_components/Molecules/HTMLRenderer/index.tsx
+++ src/app/_components/Molecules/HTMLRenderer/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import hljs from "highlight.js";
 import css from "./index.module.scss";
 import "highlight.js/styles/monokai.css";
@@ -20,22 +20,19 @@ function addLinkTargetBlank(html: string): string {
  * HTMLRenderer は、HTMLを受け取り、サニタイズ処理、タグの補正、ハイライトの適用を行うコンポーネントです。
  */
 export const HTMLRenderer = (props: Props) => {
-  let rawHTML = addLinkTargetBlank(props.rawHTML);
-
-  const htmlref = useRef<HTMLDivElement>(null);
-  // HTMLがレンダリングされたあとにHighlightを実行する
-  useEffect(() => {
-    if (htmlref.current === null) {
-      return;
-    }
-    hljs.highlightAll();
-  }, [htmlref.current?.textContent]);
-
+  let html = addLinkTargetBlank(props.rawHTML);
+  const parse = new DOMParser();
+  const dom = parse.parseFromString(html, "text/html");
+  dom.querySelectorAll("pre code").forEach((block) => {
+    const result = hljs.highlightAuto(block.textContent || "");
+    block.classList.add("hljs");
+    block.setHTMLUnsafe(result.value);
+  });
+  html = DOMPurify.sanitize(dom.body.innerHTML, {});
   return (
     <div
       className={css.markdown}
-      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(rawHTML) }}
-      ref={htmlref}
+      dangerouslySetInnerHTML={{ __html: html || "" }}
     />
   );
 };
