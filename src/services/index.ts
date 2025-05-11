43478d9f5cfaa67adc229c7154db45a622a23077
shoet <32211937+shoet@users.noreply.github.com>
2025-05-11 11:01:25 +0900

    StatusCodeによって適切なエラーページにルーティングする

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 import { API_BASE_URL } from "@/constant";
+import { notFound, unauthorized } from "next/navigation";
 
 export const getAPIPath = (path: string) => `${API_BASE_URL}${path}`;
 export class FetchError extends Error {
@@ -22,6 +23,12 @@ export const handleSuccess = async (response: Response) => {
 export const handleFailed = (error: unknown) => {
   if (error instanceof FetchError) {
     console.warn("fetch error: ", error.message);
+    if (error.status == 401) {
+      unauthorized();
+    }
+    if (error.status == 404) {
+      notFound();
+    }
   } else if (error instanceof Error) {
     console.warn("error: ", error.message, error.stack);
   } else {
