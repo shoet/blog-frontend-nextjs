3a9d607fcc36fac112fea7d30cc3a9af10bbd231
shoet <32211937+shoet@users.noreply.github.com>
2025-07-03 21:31:29 +0900

    Spinnerを実装

---
+++ src/app/_components/Atoms/Spinner/index.tsx
@@ -0,0 +1,27 @@
+import styles from './index.module.scss';
+
+export const Spinner = () => {
+  return (
+    <div className={styles.spinner}>
+      <svg
+        height={100}
+        width={100}
+        viewBox="0 0 100 100"
+        style={{
+          fill: 'none', // 塗りつぶしなし
+        }}
+      >
+        <circle
+          className={styles.circle}
+          cx={50} // 中心点のx座標
+          cy={50} // 中心点のy座標
+          r={40} // 半径 (strokeWidthが10なので、viewBox=100x100に収まるように半径が(40+10)=50になるようにしている)
+          style={{
+            stroke: "gray", // 線の色
+            strokeWidth: 10, // 線の幅
+          }}
+        />
+      </svg>
+    </div>
+  );
+};
