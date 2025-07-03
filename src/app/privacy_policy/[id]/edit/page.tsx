cb38807317c466ebfac7b38c543ba0e68d48117b
shoet <32211937+shoet@users.noreply.github.com>
2025-07-03 21:57:30 +0900

    PrivacyPolicyの編集画面を実装する

---
+++ src/app/privacy_policy/[id]/edit/page.tsx
@@ -0,0 +1,36 @@
+import { authGuard } from '@/middleware';
+import { getPrivacyPolicy } from '@/services/privacyPolicy';
+import { Metadata, ResolvingMetadata } from 'next';
+import { redirect } from 'next/navigation';
+import { ClientPrivacyPolicyEditor } from '../../_components/ClientPrivacyPolicyEditor';
+
+type PrivacyPolicyProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export const generateMetadata = async (
+  _: PrivacyPolicyProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> => {
+  const { title } = await parent;
+  return {
+    title: `プライバシーポリシー | ${title?.absolute}`,
+    description: 'プライバシーポリシー',
+  };
+};
+
+export default async function Page(props: PrivacyPolicyProps) {
+  if (!(await authGuard())) {
+    return redirect('/admin/signin');
+  }
+  const { id } = await props.params;
+  const privacyPolicy = await getPrivacyPolicy(id);
+  return (
+    <div>
+      <div>プライバシーポリシーの編集</div>
+      <ClientPrivacyPolicyEditor privacyPolicy={privacyPolicy} />
+    </div>
+  );
+}
