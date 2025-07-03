cad3ff820a2df9f4a38ab876425b42a5b2aae5cf
shoet <32211937+shoet@users.noreply.github.com>
2025-07-03 22:03:16 +0900

    PrivacyPolicyの表示画面を実装する

---
+++ src/app/privacy_policy/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { MarkdownRenderer } from '@/app/_components/Molecules/MarkdownRenderer';
+import { getPrivacyPolicy } from '@/services/privacyPolicy';
+import { Metadata } from 'next';
+
+type PrivacyPolicyProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: `プライバシーポリシー`,
+    description: 'プライバシーポリシー',
+  };
+};
+
+export default async function Page(props: PrivacyPolicyProps) {
+  const { id } = await props.params;
+  const privacyPolicy = await getPrivacyPolicy(id);
+  return <MarkdownRenderer markdown={privacyPolicy.content} />;
+}
