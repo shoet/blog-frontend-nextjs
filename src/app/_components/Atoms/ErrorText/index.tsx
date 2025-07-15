8182dac9a8d2e6d2e88671d7c5d2df52b153efe8
shoet <32211937+shoet@users.noreply.github.com>
2025-07-16 07:42:11 +0900

    ErrorTextをTailwindCSSに移行する

--- src/app/_components/Atoms/ErrorText/index.tsx
+++ src/app/_components/Atoms/ErrorText/index.tsx
@@ -1,10 +1,9 @@
 import type { ComponentProps, CSSProperties } from "react";
-import css from "./index.module.scss";
 import { theme } from "@/themes";
 
 export const ErrorText = (props: ComponentProps<"div">) => {
   const style = {
     "--text-color": theme.colors.danger,
   } as CSSProperties;
-  return <div className={css.errorText} style={style} {...props} />;
+  return <div className="text-sm text-[var(--text-color)]" {...props} style={style} />
 };
