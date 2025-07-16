0145d50d652dc3e39e666816ab225197c7a94621
shoet <32211937+shoet@users.noreply.github.com>
2025-07-16 09:20:23 +0900

    SpacerをTailwindCSSに移行する

--- src/app/_components/Atoms/Spacer/index.tsx
+++ src/app/_components/Atoms/Spacer/index.tsx
@@ -1,15 +1,16 @@
 import type { ComponentProps, CSSProperties } from "react";
-import css from "./index.module.scss";
 
 type SpacerProps = { width?: number; height?: number } & ComponentProps<"div">;
 
 export const Spacer = (props: SpacerProps) => {
   const { width, height, ...rest } = props;
-  var style: CSSProperties;
-  if (width) {
-    style = { width: `${width}px` };
-  } else {
-    style = { height: `${height}px` };
-  }
-  return <div {...rest} className={css.spacer} style={style} />;
+  const style = {
+    "--height": `${height}px`,
+    "--width": `${width}px`,
+  } as CSSProperties
+  return <div
+    {...rest}
+    className={`h-[var(--height)] w-[var(--width)] shrink-0`}
+    style={style}
+  />;
 };
