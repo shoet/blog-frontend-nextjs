b099b2a107ccd6f9e75c91c08eb1ccb4ab189883
shoet <32211937+shoet@users.noreply.github.com>
2025-07-19 06:57:55 +0900

    ToastをTailwindCSSに移行する

--- src/app/_components/Molecules/Toast/index.tsx
+++ src/app/_components/Molecules/Toast/index.tsx
@@ -1,6 +1,6 @@
 "use client";
+import clsx from "clsx";
 import { IconXmark } from "../../Atoms/Icon";
-import styles from "./index.module.scss";
 
 type Props = {
   title: string;
@@ -11,13 +11,20 @@ type Props = {
 export const Toast = (props: Props) => {
   const { title, detail, onClickClose } = props;
   return (
-    <div className={styles.toast}>
-      <div className={styles.content}>
-        <div className={styles.title}>{title}</div>
-        <div className={styles.detail}>{detail}</div>
+    <div className={clsx(
+      "flex w-[250px] flex-row items-center justify-between",
+      "rounded-md bg-[#91a679] p-2 text-gray-100 opacity-80"
+    )}>
+      <div className={clsx("flex flex-col gap-1")}>
+        <div className={clsx("font-bold text-sm")}>{title}</div>
+        <div className={clsx("text-xs")}>{detail}</div>
       </div>
       {onClickClose && (
-        <button type="button" className={styles.close} onClick={onClickClose}>
+        <button
+          type="button"
+          className={clsx("mr-2 text-white text-xl")}
+          onClick={onClickClose}
+        >
           <IconXmark />
         </button>
       )}
