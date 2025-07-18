7ebaeb6ee61bd7f3967dfa0b378a5967d832b6ec
shoet <32211937+shoet@users.noreply.github.com>
2025-07-18 09:30:14 +0900

    OverlayDialogをTailwindCSSに移行する

--- src/app/_components/Molecules/OverlayDialog/index.tsx
+++ src/app/_components/Molecules/OverlayDialog/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 import type { ReactNode } from "react";
 import { ConfirmDialog } from "../ConfirmDialog";
-import css from "./index.module.scss";
 import { IconXmark } from "../../Atoms/Icon";
+import clsx from "clsx";
 
 type OverlayDialogProps = {
   title: string;
@@ -16,21 +16,19 @@ type OverlayDialogProps = {
 
 export const OverlayDialog = (props: OverlayDialogProps) => {
   const { children, ...rest } = props;
+
   return (
-    <div className={css.overlayDialog}>
-      <button
-        type="button"
-        className={css.overlayDialogBackground}
-        onClick={rest.onClickCancel}
-      ></button>
-      <div className={css.dialog}>
+    <div className={clsx(
+      "fixed top-0 left-0 h-dvh w-dvw bg-gray-700 opacity-70",
+    )}>
+      <div className="-translate-1/2 absolute top-1/2 left-1/2">
         <ConfirmDialog {...rest}>{children}</ConfirmDialog>
         <IconXmark
-          className={css.xmark}
+          className={clsx("-top-[50px] absolute right-0 cursor-pointer")}
           size="3x"
           onClick={rest.onClickCancel}
         />
       </div>
     </div>
-  );
-};
+  )
+}
