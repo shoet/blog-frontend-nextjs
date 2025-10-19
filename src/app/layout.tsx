c55751606191b327b7aad27cdff2f8a90a9ccac9
shoet <32211937+shoet@users.noreply.github.com>
2025-10-19 11:34:59 +0900

    CloudFrontOACでPOST/PUTが動くようにする

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/app/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import Script from "next/script";
 import { ToastProvider } from "./_components/Molecules/ToastProvider";
 import { Inter } from "next/font/google";
 import { Metadata } from "next";
@@ -32,9 +31,6 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="ja" className={font.className}>
-      <head>
-        <Script src="/register-service-worker.js" />
-      </head>
       <body>
         <ToastProvider>{children}</ToastProvider>
       </body>
